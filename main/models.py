d0437c7eb6acbed51b5528cec3d487bc6c718012
mohamadhos78 <mohamadhos88@gmail.com>
2022-08-05 12:43:13 +0430

    add reply to talk model & chanage name of Talk to delneshtesh

--- main/models.py
+++ main/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Course(models.Model):
     name = models.CharField(max_length=128, null=False, blank=False)
     description = models.CharField(max_length=512, null=True, blank=True)
-
+    
     def __str__(self):
         return self.name
 
@@ -28,10 +28,15 @@ class Talk(models.Model):
     talk = models.TextField(null=False, blank=False)
     date = models.DateField(auto_now_add=True)
     is_published = models.BooleanField(default=False)
+    reply = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')
     
     def __str__(self):
         return self.name
     
+    class Meta:
+        verbose_name = 'دلنوشته'
+        verbose_name_plural = 'دلنوشته ها'
+
 
 class Teacher(models.Model):
     username = models.CharField(max_length=64, null=False, blank=False)
