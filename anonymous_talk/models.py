beaed2c9b874d78820726bdc80d342f3617c0003
mohamadhos78 <mohamadhos88@gmail.com>
2022-09-03 11:35:58 -0700

    add reply field & add verbose name  to AnonymousTalk Model

--- anonymous_talk/models.py
+++ anonymous_talk/models.py
@@ -3,6 +3,11 @@ from django.db import models
 # Create your models here.
 class AnonymousTalk(models.Model):
     talk = models.TextField(null=False, blank=False)
+    reply = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')
 
     def __str__(self):
         return self.talk[:40]
+
+    class Meta:
+        verbose_name = 'پیام ناشناس'
+        verbose_name_plural = 'پیام های ناشناس' 
