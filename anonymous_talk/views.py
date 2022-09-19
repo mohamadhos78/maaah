bc5ed9f981cf0245bd4cc906661ab32bf560a80c
mohamadhos78 <mohamadhos88@gmail.com>
2022-09-19 12:07:36 -0700

    add messaging for talk and answers

--- anonymous_talk/views.py
+++ anonymous_talk/views.py
@@ -1,19 +1,42 @@
 from django.shortcuts import render
 from .forms import AnonymousTalkForm
 from .models import AnonymousTalk
+from django.contrib import messages
 
 def send_message(request):
     if request.method == "POST":
         form = AnonymousTalkForm(request.POST)
         if form.is_valid():
             data = form.save()
-            print(data.id)
+        messages.success(request, 
+            f"""پیام شما با موفقیت برای حاج اقا ارسال شد،
+            شما می‌توانید از طریق {data.id} پاسخ حاج اقا را مشاهده کنید
+            """
+        )
     return render(request, "say-anonymous.html")
 
 def see_answer(request):
     id = request.POST.get('message_id', None)
     if id:
-        answer = AnonymousTalk.objects.get(id=id)
+        try:
+            answer = AnonymousTalk.objects.get(id=id)
+        except Exception as error:
+            answer = None
+            messages.error(
+                request,
+                "کد وارد شده صحیح نمی‌باشد"
+                )
+        else:
+            if answer.reply:
+                messages.success(
+                    request, 
+                    "نبریک میگم، حاج اقا به پیامت جواب داده"
+                )
+            else:
+                messages.info(
+                    request,
+                    "متاسفانه هنوز به پیام شما پاسخی داده نشده است"
+                    )
     else:
         answer = None
         
