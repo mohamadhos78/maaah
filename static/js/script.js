64d847ff6f0da37b62e8e4bbff6c91ac569cca01
jalali23 <73940488+jalali23@users.noreply.github.com>
2022-09-20 22:29:48 +0430

    Edit the answer pages of Haj Agha and Anonymous Say

--- static/js/script.js
+++ static/js/script.js
@@ -1,23 +1,5 @@
 $(document).ready(function(){
-		$('.responsive-menu-icon').click(function(){
-		var responsiveMenu = $('.responsive-menu');
-		responsiveMenu.show();
-		$('.profile-pic').hide();
-		responsiveMenu.animate({
-			right: 0
-		},400);
-		$('body').append('<div class="back-container"></div>');
-		$('.back-container').click(function(){			
-			var responsiveMenu = $('.responsive-menu');	
-			responsiveMenu.animate({
-				right: '-220px'
-			},400,function(){
-				responsiveMenu.hide();	
-				$('.profile-pic').show();		
-			});
-			$(this).remove();			
-		});
-	});
+
 	$('.accordion-heading').click(function(){
 		if($(this).attr('data-open') == 'open'){
 			$('.accordion-body').slideUp("slow");
